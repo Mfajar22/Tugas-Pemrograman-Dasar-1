a0afe3bcc95fcd210c9d91faa83759719269d248
WINDOWS 10 <WINDOWS 10@DESKTOP-K1AUPQN>
2024-09-28 20:35:05 +0700

    Add project files.

---
+++ Tugas Pemrograman Dasar 1/Tugas Pemrograman Dasar 1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+	double hargaAsli = 150000;
+	double presentaseDiskon = 12.5;
+
+	double jumlahDiskon = hargaAsli * (presentaseDiskon / 100);
+	double hargaAkhir = hargaAsli - jumlahDiskon;
+
+	std::cout << " Harga : " << hargaAsli << " ribu " << std::endl;
+	std::cout << " Diskon : " << presentaseDiskon << std::endl;
+	std::cout << " Besarnya diskon dari harga : " << jumlahDiskon << std::endl;
+	std::cout << " Harga setelah diskon : " << hargaAkhir << " ribu " << std::endl;
+
+	return 0;
+
+}
